ce59719b87efbb2c41408358a1f8a5bea5b2adc5
Marco Fernandes <marcospiridon@yahoo.com>
2025-07-16 16:58:15 +0100

    update camera

--- camera.py
+++ camera.py
@@ -1,8 +1,23 @@
-from picamzero import Camera
-import os
+#!/usr/bin/python3
 
-home_dir = os.environ['HOME'] #set the location of your home directory
-cam = Camera()
-cam.start()
-cam.take_photo(f"{home_dir}/dev/robot/new_image.jpg") #save the image to your desktop
-cam.stop()
+# Capture a JPEG while still running in the preview mode. When you
+# capture to a file, the return value is the metadata for that image.
+
+import time
+
+from picamera2 import Picamera2, Preview
+
+picam2 = Picamera2()
+
+preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
+picam2.configure(preview_config)
+
+picam2.start_preview(Preview.QTGL)
+
+picam2.start()
+time.sleep(2)
+
+metadata = picam2.capture_file("test.jpg")
+print(metadata)
+
+picam2.close()
