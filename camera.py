f400c67b86e237cb91fbb1e0f5fc40000dc89a89
Marco Fernandes <marcospiridon@yahoo.com>
2025-07-16 16:08:15 +0100

    update camera

--- camera.py
+++ camera.py
@@ -1,11 +1,9 @@
-# Example using picamera2 (newer library)
-from picamera2 import Picamera2, Preview
-import time
+from picamzero import Camera
+import os
 
-picam2 = Picamera2()
-camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)})
-picam2.configure(camera_config)
-picam2.start_preview(Preview.QTGL)
-time.sleep(2)
-picam2.capture_file("dev/robot/image2.jpg")
-picam2.close()
+home_dir = os.environ['HOME'] #set the location of your home directory
+cam = Camera()
+
+cam.start_preview()
+cam.take_photo(f"{home_dir}/dev/robot/new_image.jpg") #save the image to your desktop
+cam.stop_preview()
