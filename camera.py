0ab1280f546683e80a32127b870f7361ac63fdf0
Marco Fernandes <marcospiridon@yahoo.com>
2025-07-16 16:28:23 +0100

    start and stop

--- camera.py
+++ camera.py
@@ -3,5 +3,6 @@ import os
 
 home_dir = os.environ['HOME'] #set the location of your home directory
 cam = Camera()
-
+cam.start()
 cam.take_photo(f"{home_dir}/dev/robot/new_image.jpg") #save the image to your desktop
+cam.stop()
